c096457d7b5ded5a96b2c46f925e6e75b0e3c186
smithrowe-10 <smj0418smj@gmail.com>
2025-02-25 21:45:32 +0900

    feat:20250225 React lesson started

---
+++ vite-project/src/App2.jsx
@@ -0,0 +1,11 @@
+function App2() {
+  return (
+  <>
+    <h1>Hello World</h1>
+    <h2>첫 번째 리액트 컴포넌트입니다.</h2>
+  </>
+  );
+}
+
+export default App2
+
---
+++ vite-project/src/example3.js
@@ -0,0 +1,15 @@
+// Javascript 복습 및 React 활용 문법 # 3
+
+// 클래스와 상속
+
+/**
+ * 기본적으로 JS(ES6)에서의 클래스의 정의 Java와 유사함
+ * 클래스 정의 방식
+ */
+class Person{
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+

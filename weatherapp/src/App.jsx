b57168fa49211671b6d44b3e40ac00e84ab2724f
rdy2rck <sun19965@gmail.com>
2025-03-06 20:52:58 +0900

    feat:20250306 githubapiapp started

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -1,11 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  return (
-    <>
-    </>
-  )
+
+  const [weather, setWeather] = useState({
+    temp: '',
+    desc: '',
+    icon: '',
+  });
+
+  useEffect(() => {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Seoul&units=Metric&APIkey=e651216af5b205d07a29b3b2b3a131b6')
+    .then(response => response.json())
+    .then(result => {
+      setWeather({
+        temp: result.main.temp,
+        desc: result.weather[0].description,
+        icon: result.weather[0].icon
+      })
+    })
+    .catch(err => console.log(err))
+  }, []);
+
+  if(weather.icon) {
+    return (
+      <>
+        <p>온도 : {weather.temp} 도</p>
+        <p>설명 : {weather.desc}</p>
+        <img src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt='날씨 아이콘' />
+      </>
+    );
+  } 
+  else{
+    return <div>로딩중...</div>
+  }
 }
 
 export default App

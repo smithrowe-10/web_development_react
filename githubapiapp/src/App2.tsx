f4a3ac34cd3648db6a3de3e8103d0bce9ebdd261
smithrowe-10 <smj0418smj@gmail.com>
2025-03-10 19:30:19 +0900

    feat: 20250310 shoppinglist started

--- githubapiapp/src/App2.tsx
+++ githubapiapp/src/App2.tsx
@@ -1,10 +1,64 @@
+// import { AgGridReact } from 'ag-grid-react';
+// import 'ag-grid-community/styles/ag-grid.css';
+// import 'ag-grid-community/styles/ag-theme-material.css';
+// import { ColDef } from 'ag-grid-community';
+// import { useState } from 'react';
+// import axios from 'axios';
+// import './App.css';
+
+// type Repository = {
+//   id: number;
+//   full_name: string;
+//   html_url: string;
+// };
+
+// function App() {
+//   const [ keyword, setKeyword ] = useState('');
+//   const [ repodata, setRepodata ] = useState<Repository[]>([]);
+
+//   const handleClick = () => {
+//     axios.get<{ items: Repository[]}>(`https://api.github.com/search/repositories?q=${keyword}`)
+//     .then(response => setRepodata(response.data.items))
+//     .catch(err => console.log(err))
+//   }
+
+//   const [columnDefs] = useState<ColDef[]>([
+//     {field: 'id'},
+//     {field: 'full_name'},
+//     {field: 'html_url'},
+//   ]);
+
+//   return (
+//     <>
+//       <input 
+//         value={keyword}
+//         onChange={e => setKeyword(e.target.value)}
+//       />
+//       <br /> <br />
+//       <button onClick={handleClick}>Search</button>
+//       <div className="ag-theme-material"
+//         style={{height: 500, width: 850}}>
+//         <AgGridReact
+//           rowData={repodata} 
+//           columnDefs={columnDefs}
+//         />
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App
+
 import { AgGridReact } from 'ag-grid-react';
+import { ClientSideRowModelModule, ValidationModule } from 'ag-grid-community'; // 여기도 추가된 부분
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-material.css';
-import { ColDef } from 'ag-grid-community';
+import { ColDef, ModuleRegistry } from 'ag-grid-community'; // ag-grid-community
 import { useState } from 'react';
 import axios from 'axios';
 import './App.css';
+
+ModuleRegistry.registerModules([ClientSideRowModelModule, ValidationModule]); // -> 모듈을 명시적으로 등록
 
 type Repository = {
   id: number;
@@ -13,38 +67,44 @@ type Repository = {
 };
 
 function App() {
-  const [ keyword, setKeyword ] = useState('');
-  const [ repodata, setRepodata ] = useState<Repository[]>([]);
+  const [keyword, setKeyword] = useState('');
+  const [repodata, setRepodata] = useState<Repository[]>([]);
 
   const handleClick = () => {
-    axios.get<{ items: Repository[]}>(`https://api.github.com/search/repositories?q=${keyword}`)
-    .then(response => setRepodata(response.data.items))
-    .catch(err => console.log(err))
-  }
+    axios
+      .get<{ items: Repository[] }>(
+        `https://api.github.com/search/repositories?q=${keyword}`
+      )
+      .then((response) => setRepodata(response.data.items))
+      .catch((err) => console.log(err));
+  };
 
   const [columnDefs] = useState<ColDef[]>([
-    {field: 'id'},
-    {field: 'full_name'},
-    {field: 'html_url'},
+    { field: 'id', sortable: true, filter: true},
+    { field: 'full_name' },
+    { field: 'html_url' },
   ]);
 
   return (
     <>
-      <input 
-        value={keyword}
-        onChange={e => setKeyword(e.target.value)}
-      />
+      <input value={keyword} onChange={(e) => setKeyword(e.target.value)} />
       <br /> <br />
       <button onClick={handleClick}>Search</button>
-      <div className="ag-theme-material"
-        style={{height: 500, width: 850}}>
+      <div
+        className="ag-theme-material"
+        style={{ height: 500, width: 850 }}
+      >
         <AgGridReact
-          rowData={repodata} 
+          theme="legacy"
+          rowData={repodata}
           columnDefs={columnDefs}
+          modules={[ClientSideRowModelModule, ValidationModule]}  // 속성 및 속성값을 추가
+          pagination={true}
+          paginationPageSize={10}
         />
       </div>
     </>
   );
 }
 
-export default App
+export default App;

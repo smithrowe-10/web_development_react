a3754ac15d28512d4952ba4ccb26b259f9257d7a
smithrowe-10 <smj0418smj@gmail.com>
2025-03-19 21:41:48 +0900

    feat: 20250319 useReducer login form

--- usereducerapp/src/App.jsx
+++ usereducerapp/src/App.jsx
@@ -1,12 +1,63 @@
+import { useReducer } from 'react';
 import './App.css'
+import LoginForm from './components/LoginForm';
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case "LOGIN_SUCCESS":
+      return {
+        ...state,
+        user: action.payload,
+        isLogin: true,
+        message: '로그인 성공했습니다😋',
+      }
+    case "MISS_ID":
+      return {
+        ...state,
+        isLogin: false,
+        message: '아이디를 잘못 입력했습니다😭',
+      }
+    case "MISS_PASSWORD":
+      return{
+        ...state,
+        isLogin: false,
+        message: '비밀번호를 잘못 입력했습니다😰'
+      }
+    case "LOGIN_FAILURE":
+      return{
+        ...state,
+        isLogin: false,
+        message: 'ID & PASSWORD not accurate👻💀',
+      };
+    case "LOGOUT":
+      return{
+        ...state,
+        isLogin: false,
+        message: '로그아웃 성공했습니다✋🏼😥',
+      };
+    default:
+      return{
+        ...state
+      };
+  }
+};
 
 function App() {
 
+  const [state, dispatch] = useReducer(reducer, { isLogin: false, message : '',})
+
   return (
-    <>
-      
-    </>
+    <div>
+      {state.isLogin ? (
+        <>
+          <strong>welcome😀</strong>
+          <button onClick={() => dispatch({type: "LOGOUT"})}>로그아웃</button>
+        </>
+      ) : (
+      <LoginForm state={state} dispatch={dispatch} />
+      )}
+    </div>
   )
 }
 
-export default App
+export default App;
---
+++ usereducerapp/src/App3.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+import LoginForm from './components/LoginForm';
+
+function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
+  return (
+    <div>
+      {isLogin ? (
+        // true일 때 로그인 환영합니다
+        <div>
+          <strong>환영합니다🍕</strong>
+          <button onClick={() => setIsLogin(!isLogin)}>로그아웃</button>
+        </div>
+      ) : (
+        // false일 때 나타나는 로그인 페이지
+        <LoginForm setIsLogin={setIsLogin} />
+      )}
+    </div>
+  )
+}
+
+export default App

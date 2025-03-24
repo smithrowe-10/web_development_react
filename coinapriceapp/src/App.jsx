cf607d2e6e947094c500315b9ec849a9c95cb78a
smithrowe-10 <smj0418smj@gmail.com>
2025-03-24 20:11:14 +0900

    feat: movie app started

--- coinapriceapp/src/App.jsx
+++ coinapriceapp/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true); 
+  const [coins, setCoins] = useState([]);
 
+  // setLoading(false); 를 적용하게 되는 시점이 언제인가?
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+    .then(response => response.json())
+    // .then(json => console.log(json));
+    .then(json => {
+      setCoins(json)
+      setLoading(false);
+    })
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Coin Price Tracker</h1>
+      {loading ? <strong>Loading...🎞</strong> : null}
+    </div>
   )
 }
 
-export default App
+export default App;
